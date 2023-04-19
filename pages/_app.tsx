3b8d5367a48126367ce26a1e569d771276edc11d
David Gaynor <runnedrun@gmail.com>
2023-04-19 11:55:39 +0200

    fix auth bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ import "../styles/globals.css"
 import { logNavSteps } from "@/analytics/navigationSteps"
 
 init()
-// initAuth()
+initAuth()
 
 const clientSideEmotionCache = createEmotionCache()
 
